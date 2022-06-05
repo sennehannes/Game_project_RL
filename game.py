a0bab66e414c19ac013dc9074dc415a1e29b24d3
sennehannes <senne.hannes@hotmail.com>
2022-06-05 13:43:15 +0200

    patch 1.1.0 added screen

--- game.py
+++ game.py
@@ -1,11 +1,31 @@
 import pygame
 
-def run():
+class game():
 
     def __init__(self, screen, target_fps):
         self.screen = screen
+        self.width = self.screen.get_size()[0]
+        self.height = self.screen.get_size()[1]
+        self.target_fps = target_fps
+        self.running = True
+        self.paused = False
+
+    def run(self):
+        while(self.running):
+            for event in pygame.event.get():
+                  if event.type == pygame.QUIT:
+                      self.running = False
+            pygame.display.update()
+        
 
 
 
 if __name__ == '__main__':
-    run()
+    pygame.display.init()
+    disp_size = (1280,720)
+    screen = pygame.display.set_mode(disp_size)
+    pygame.display.set_caption('adventure')
+    game(screen,60).run()
+
+    pygame.quit
+    exit()
