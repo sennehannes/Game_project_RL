4ffbe513bffb28e89c8f1a19c39790ef12a5c97a
sennehannes <senne.hannes@hotmail.com>
2022-06-05 19:44:29 +0200

    patch 1.1.0 first button interactions

--- game.py
+++ game.py
@@ -7,25 +7,35 @@ class game():
         self.width = self.screen.get_size()[0]
         self.height = self.screen.get_size()[1]
         self.target_fps = target_fps
+        self.screen_color=(255,255,255)
         self.running = True
         self.paused = False
 
     def run(self):
+        screen.fill(self.screen_color)
+        image = pygame.image.load("sprites/characters/player.png")
+        screen.blit(image, dest = (0,0))
         while(self.running):
             for event in pygame.event.get():
-                  if event.type == pygame.QUIT:
-                      self.running = False
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.KEYDOWN:
+                    print("a key has been pressed")
+                    if event.key == pygame.K_RSHIFT:
+                        print("pressed entre")
+                    if event.key == pygame.K_m:
+                        print("m has been pressed")
             pygame.display.update()
         
-
-
-
 if __name__ == '__main__':
-    pygame.display.init()
+    pygame.init()
+    is_initialized = pygame.get_init()
+    while is_initialized == False:
+        pygame.init()
+        print("reinitialize pygame")
     disp_size = (1280,720)
     screen = pygame.display.set_mode(disp_size)
     pygame.display.set_caption('adventure')
     game(screen,60).run()
-
     pygame.quit
     exit()
